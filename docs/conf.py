10c00080e72d5b398a64f26479d42f7d0023d223
ccoffrin <carleton@coffrin.com>
2018-05-26 16:18:18 -0600

    adding sphinx.ext.napoleon for google style doc strings

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
 
 autoclass_content = 'both'
 
